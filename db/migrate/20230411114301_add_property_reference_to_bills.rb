a01e985d81cea5c42f2b1eee6df98c74168bd0e9
thanhhai45 <thanhhai82451495@gmail.com>
2023-04-11 23:00:34 +0700

    Revert "08 Setup devise for authentication"

--- db/migrate/20230411114301_add_property_reference_to_bills.rb
+++
@@ -1,5 +0,0 @@
-class AddPropertyReferenceToBills < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :bills, :property, null: false, foreign_key: true
-  end
-end
--- db/migrate/20230411114836_add_property_reference_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPropertyReferenceToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :users, :property, null: false, foreign_key: true
-  end
-end
